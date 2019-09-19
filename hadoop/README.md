1c5b3e7dd7f26f77f6209f324435b7d099ddcb06
shizk233 <wangwangdaxian233@gmail.com>
2019-09-19 21:52:17 +0800

    添加Hadoop部署

---
+++ hadoop/README.md
@@ -0,0 +1,4 @@
+**dependency**: ssh service running and ssh localhost as nopassword
+
+
+HADOOP native library 在Mac下不可用，需要手动编译，暂时跳过。
---
+++ hadoop/install-hadoop.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+version="hadoop-2.7.7"
+PROJECT_HOME="$HOME/apache"
+HADOOP_HOME="/usr/local/$version"
+# download hadoop release
+function download() {
+  curl -s "http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/$version/$version.tar.gz" -o "$version.tar.gz"
+  tar -zxf "$version.tar.gz" -C "./$version"
+  sudo mv "./$version" /usr/local
+  rm "$HADOOP_HOME/etc/hadoop/*.cmd" "$HADOOP_HOME/bin/*.cmd" "$HADOOP_HOME/sbin/*.cmd"
+  rm "$version.tar.gz"
+}
+# setup hadoop configuration
+function ln_config() {
+  for file in "$@"; do
+    rm -f "$HADOOP_HOME/etc/hadoop/$file"
+    ln -s "$PROJECT_HOME/hadoop/etc/hadoop/$file" "$HADOOP_HOME/etc/hadoop/$file"
+  done
+}
+function ln_all() {
+  ln_config core-site.xml hdfs-site.xml mapred-site.xml yarn-site.xml hadoop-env.sh
+}
+# init hdfs
+function init() {
+  hdfs namenode -format
+  start-dfs.sh
+  hdfs dfs -mkdir -p "/user/$USER"
+  stop-dfs.sh
+}
