d5ecb2f65984bfb351ba7f07a452d6d421eccaec
shizk233 <wangwangdaxian233@gmail.com>
2019-09-26 15:05:19 +0800

    安装部署Flink

---
+++ flink/install-flink.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+release="flink-1.9.0"
+FLINK_HOME="/usr/local/$release"
+# download flink release
+function download() {
+    curl "https://mirrors.tuna.tsinghua.edu.cn/apache/flink/$release/$release-bin-scala_2.12.tgz" -o "$release.tar.gz"
+    sudo tar -zxf "$release.tar.gz" -C /usr/local
+    sudo chown -R "$USER" "$FLINK_HOME"
+    rm $FLINK_HOME/bin/*.bat
+    rm "$release.tar.gz"
+}
