78c083e5e1e7ef2aed604f9a7fa67cc5c2723b88
shizk233 <wangwangdaxian233@gmail.com>
2020-01-09 00:13:15 +0800

    Hadoop添加hadoop-env.sh配置

--- hadoop/README.md
+++ hadoop/README.md
@@ -6,3 +6,5 @@ HADOOP native library 没有适合MacOS的预编译版本，忽略相关警告�
 MacOS下由于JAVA_HOME会通过/usr/libexe/java_home自动识别最高版本，
 并且Hadoop SSH执行命令不经过登陆脚本（.zshrc等），所以要么系统最高装java1.8，
 要么直接在hadoop-env.sh中写死JAVA_HOME路径。
+
+本机安装还是写死方便QAQ
--- hadoop/setup-hadoop.sh
+++ hadoop/setup-hadoop.sh
@@ -6,7 +6,7 @@ source "$APACHE_HOME"/common/setup.sh
 function ln_all() {
   export TARGET_PATH="$HADOOP_HOME"/etc/hadoop
   export SOURCE_PATH="$APACHE_HOME"/hadoop/etc/hadoop/
-  link_file core-site.xml hdfs-site.xml mapred-site.xml yarn-site.xml
+  link_file core-site.xml hdfs-site.xml mapred-site.xml yarn-site.xml hadoop-env.sh
 }
 
 # Init hdfs
